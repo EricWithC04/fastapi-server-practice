776f65b5136987d23bcf4f55ccad8a78bc5704cb
EricWithC04 <negativegame117@gmail.com>
2024-05-17 14:47:12 -0300

    feat: add route post to execute code

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class CodeBody(BaseModel):
+    code: str
 
 @app.get("/")
 def read_root():
     return "Hello World!"
+
+@app.post("/execute")
+def execute_code(body_code: CodeBody):
+    exec(body_code.code)
+    return "Code Executed Successfully"

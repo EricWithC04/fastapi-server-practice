cc66107981f4c5a2402ad385679c058950e258f1
EricWithC04 <negativegame117@gmail.com>
2024-08-09 15:58:55 -0300

    fix: context and response

--- stream_data.py
+++ stream_data.py
@@ -13,8 +13,8 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
             
             history = [
-                {"role": "system", "content": "You are an intelligent assistant. You always provide well-reasoned answers that are both correct and helpful."},
-                {"role": "user", "content": "Hello, introduce yourself to someone opening this program for the first time. Be concise."},
+                {"role": "system", "content": "Eres un tutor inteligente de la asignatura '''Python para ciencias de datos'''', debes responder siempre preguntas relacionadas con python y la ciencias de datos. '''No respondas nada que no tenga que ver con python'''. Dame resupuestas cortas no superes los 50 caracteres e ignora las preguntas de seguimiento."},
+                # {"role": "user", "content": "Presentate la primera vez que se habra el programa"},
                 {"role": "user", "content": data},
             ]
             
@@ -27,10 +27,14 @@ async def websocket_endpoint(websocket: WebSocket):
             )
 
             new_message = {"role": "assistant", "content": ""}
+
+            completed_text = ""
+
             for chunk in completion:
                 if chunk.choices[0].delta.content:
                     # Enviar cada fragmento al cliente
-                    await websocket.send_text(chunk.choices[0].delta.content)
+                    completed_text += chunk.choices[0].delta.content
+                    await websocket.send_text(completed_text)
                     new_message["content"] += chunk.choices[0].delta.content
 
             history.append(new_message)
